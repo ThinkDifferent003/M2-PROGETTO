63ac9d924c8d402fc846e07f4269353b77942fec
ThinkDifferent003 <rolandbibal3@gmail.com>
2025-11-20 18:51:10 +0100

    sistemato un pochino

--- M2 Progetto/Assets/Scripts/M1ProjectTest.cs
+++ M2 Progetto/Assets/Scripts/M1ProjectTest.cs
@@ -8,8 +8,7 @@ public class M1ProjectTest : MonoBehaviour
 
     [SerializeField] private Hero a;
     [SerializeField] private Hero b;
-    private bool finishBattle = false;
-   
+    private int state = 0;
 
     
 
@@ -21,87 +20,55 @@ public class M1ProjectTest : MonoBehaviour
 
     public void MicioAttack()
     {
-        Debug.Log($"L'Eroe {a.GetName()} sta attaccando L'Eroe {b.GetName()}");
-        if (GameFormulas.HasHit(a.GetBaseStats(), b.GetBaseStats()))
+        Debug.Log($"L'Eroe {a.GetName()} sta attaccando l'Eroe {b.GetName()}");
+        if (!GameFormulas.HasHit(a.GetBaseStats(), b.GetBaseStats()))
         {
-            Debug.Log($"L'attacco va a segno");
-
-            if (a.GetWeapon().GetElement() == b.GetWeakness())
-            {
-                Debug.Log($"WEAKNESS L'attacco dell'Eroe {a.GetName()} è superefficace");
-            }
-            else if (a.GetWeapon().GetElement() == b.GetResitance())
-            {
-                Debug.Log($"RESIST L'attacco dell'Eroe {a.GetName()} è poco efficace");
-            }
-
-            int damage;
-            damage = GameFormulas.CalculateDamage(a, b);
-            Debug.Log($"L'eroe {a.GetName()} infligge a Eroe {b.GetName()} la bellezza di {damage} danni");
-
-            b.TakeDamage(damage);
-            Debug.Log($"L'eroe {b.GetName()} riceve {damage} danni da parte dell' Eroe {a.GetName()} e gli rimangono {b.GetHp()} HP");
-
-
-            if (!b.IsAlive())
-            {
-                Debug.Log($"L'eroe {a.GetName()} ha VINTO");
-                finishBattle = true;
-                return;
-            }
-            else if (b.IsAlive())
-            {
-                Debug.Log($"L'eroe {b.GetName()} è sopravvisuto");
-                CaneAttack();
-            }
+            Debug.Log($"L'Eroe {b.GetName()} ha schivato il colpo");
+            state = 2;
+            return;
+        }
+        int damage = GameFormulas.CalculateDamage(a , b);
+        Debug.Log($"L'Eroe {a.GetName()} ha sferrato {damage} danni");
+        b.TakeDamage(damage);
+        Debug.Log($"L'Eroe {b.GetName()} ha ricevuto {damage} danni!");
+        Debug.Log($"HP rimanenti : {b.GetHp()}");
+        if (!b.IsAlive())
+        {
+            Debug.Log($"L'Eroe {a.GetName()} ha VINTO");
+            state = 3;
         }
         else
         {
-            CaneAttack();
+            state = 2;
         }
     }
 
     public void CaneAttack()
     {
-        Debug.Log($"L'Eroe {b.GetName()} sta attaccando L'Eroe {a.GetName()}");
-        if (GameFormulas.HasHit(b.GetBaseStats(), a.GetBaseStats()))
-
+        Debug.Log($"L'eroe {b.GetName()} sta attaccando l'Eroe {a.GetName()}");
+        if (!GameFormulas.HasHit(b.GetBaseStats(), a.GetBaseStats()))
         {
-            if (b.GetWeapon().GetElement() == a.GetWeakness())
-            {
-                Debug.Log($"WEAKNESS L'attacco dell'Eroe {b.GetName()} è superefficace");
-            }
-            else if (b.GetWeapon().GetElement() == a.GetResitance())
-            {
-                Debug.Log($"RESIST L'attacco dell'Eroe {b.GetName()} è poco efficate");
-            }
-            int damage;
-            damage = GameFormulas.CalculateDamage(b, a);
-            Debug.Log($"L'eroe {b.GetName()} infligge a Eroe {a.GetName()} la bellezza di {damage} danni");
-
-            a.TakeDamage(damage);
-            Debug.Log($"L'eroe {a.GetName()} riceve {damage} danni da parte dell' Eroe {b.GetName()} e gli rimangono {a.GetHp()} HP");
-
-
-            if (!a.IsAlive())
-            {
-                Debug.Log($"L'eroe {b.GetName()} ha VINTO");
-                finishBattle = true;
-                return;
-            }
-            else if (a.IsAlive())
-            {
-                Debug.Log($"L'Eroe {a.GetName()} è sopravvisuto");
-                MicioAttack();
-            }
+            Debug.Log($"L'Eroe {a.GetName()} ha schivato il colpo");
+            state = 1;
+            return;
+        }
+        int damage = GameFormulas.CalculateDamage(b , a);
+        Debug.Log($"L'Eroe {b.GetName()} ha sferrato {damage} danni");
+        a.TakeDamage(damage);
+        Debug.Log($"L'Eroe {a.GetName()} ha ricevuto {damage} danni!");
+        Debug.Log($"HP rimanenti . {a.GetHp()}");
+        if (!a.IsAlive())
+        {
+            Debug.Log($"L'eroe {b.GetName()} ha VINTO");
+            state = 3;
         }
         else
         {
-            MicioAttack();
+            state = 1;
         }
     }
 
-   
+    
 
     public void WhoStart()
     {
@@ -125,12 +92,33 @@ public class M1ProjectTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!finishBattle)
+        
+         switch (state)
         {
-            WhoStart();
-            MicioAttack( );
-
+            case 0:
+                WhoStart();
+                if (a.GetBaseStats().spd +a.GetWeapon().GetStats().spd >= b.GetBaseStats().spd + b.GetWeapon().GetStats().spd)
+                {
+                    state = 1;
+                }
+                else
+                {
+                    state = 2;
+                }
+                break;
+            case 1:
+                MicioAttack();
+                break;
+            case 2:
+                CaneAttack();
+                break;
+            case 3:
+                Debug.Log("Fine");
+                break;
         }
+            
+
+        
        
     }
 }
